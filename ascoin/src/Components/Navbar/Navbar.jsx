e36b89da148e611eae6fbcca8fa236309e74316e
Cristian-Araya <49296784+Cristian-Araya@users.noreply.github.com>
2021-02-28 12:53:24 -0300

    Create Main Page
    Se eliminaron archivos inecesarios y se crean los componentes basicos para la main Page

---
+++ ascoin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Logo</a>
+                <ul className="nav justify-content-end">
+                    <li className="nav-item">
+                        home
+                    </li>
+                </ul>
+          </div>
+        </nav>
+    )
+}
+
+export default Navbar
